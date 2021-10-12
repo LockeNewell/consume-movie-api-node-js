1aa2e7b40ad176e6a4d9ddc9a811b46339f221eb
Locke J Newell <Locke.Newell@gmail.com>
2021-10-12 13:01:39 -0500

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+let substr = 'spiderman';
+let url = `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}`;
+let task = ['consume api at the url for a given substring',
+            'create a list of titles',
+            'sort the list of titles'
+        ];
+let note = 'the the object returned has a page and total_page var';
+let sampleOutput = 'is in movie.stream';
